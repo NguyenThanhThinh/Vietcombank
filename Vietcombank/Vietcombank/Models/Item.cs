52f163f2c3b3fc9be66cdcf30073c23f31050f12
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-10-17 13:24:57 +0700

    add ennity

---
+++ Vietcombank/Vietcombank/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace Vietcombank.Models
+{
+    public class Item
+    {
+        public string type { get; set; }
+        public string imageurl { get; set; }
+        public string muatienmat { get; set; }
+        public string muack { get; set; }
+        public string bantienmat { get; set; }
+        public string banck { get; set; }
+    }
+}
